4641956cbc649dd443dc0af3b5cf6125191d2de6
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-09-12 15:12:52 +0530

    adding followed tutorial

--- harmoniq-react/src/App.js
+++ harmoniq-react/src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import {Amplify} from 'aws-amplify'
 import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
-// import awsExports from './aws-exports';
-// Amplify.configure(awsExports);
+import Quiz from './Quiz';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
           <main>
             <header className='App-header'>
               {/* Quiz Component */}
+              <Quiz/>
               {/* Sign Out Button */}
+
               <button 
                 onClick={signOut} 
                 style={{ 
